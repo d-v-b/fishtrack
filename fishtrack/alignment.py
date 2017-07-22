f097b6cf86d4ca8ac4e9a0294d4fa40d2b7c94ff
d-v-b <vaslav.tchitcherine@gmail.com>
2017-07-22 13:55:04 -0400

    no longer crop final aligned image with brain mask

--- fishtrack/alignment.py
+++ fishtrack/alignment.py
@@ -3,9 +3,9 @@ from .util import long_tail_threshold, cart2pol, angle_difference
 from .measurements import eigenvalues, remotest_point
 
 
-def align_brains(static, moving):
+def align_brains(static, moving, brain_position):
     from dipy.align.imaffine import AffineRegistration, AffineMap
-    from dipy.align.transforms import RigidTransform2D
+    from dipy.align.transforms import RigidTransform2D 
     from scipy.ndimage.measurements import center_of_mass
     from numpy import round, eye, array
 
@@ -13,7 +13,6 @@ def align_brains(static, moving):
     rigid = RigidTransform2D()
 
     static_, moving_ = static.copy(), moving.copy()
-    coms = (0, 0)
 
     static_brain = get_brain(static_, return_sparse=False)
     moving_brain = get_brain(moving_, return_sparse=False)
@@ -21,29 +20,30 @@ def align_brains(static, moving):
     fill_value = static[static_brain].min()
 
     if not moving_brain.any():
-        return moving_, coms, AffineMap(eye(3))
+        return moving_, AffineMap(eye(3))
 
     # isolate region of interest for registration
-    coms = array(round(center_of_mass(static_brain)).astype('int'))
-    static_brain_cropped = (static_brain * static)[coms[0] - 20:coms[0] + 20, coms[1] - 30:coms[1] + 30]
-    moving_brain_cropped = (moving_brain * moving)[coms[0] - 20:coms[0] + 20, coms[1] - 30:coms[1] + 30]
-
-    # set background values to minimum of brain to mitigate effect of masking on registration
-    static_brain_cropped[static_brain_cropped == 0] = static_[static_brain].min()
-    moving_brain_cropped[moving_brain_cropped == 0] = moving_[moving_brain].min()
+    w_y = slice(brain_position[0] -40, brain_position[0] + 40)
+    w_x = slice(brain_position[1] -50, brain_position[1] + 50)
+    static_brain_cropped = static_[w_y,w_x]
+    moving_brain_cropped = moving_[w_y,w_x]
 
     # estimate rigid transformation between static and moving
     g2w = eye(3)
     g2w[:2, -1] = -array(static_brain_cropped.shape) / 2
-
+    
+    # first align centers of mass in the field of view
+    c_of_mass = transform_centers_of_mass(static_brain_cropped, g2w, moving_brain_cropped, g2w) 
     params0 = None
-    starting_affine = None
+    
+    starting_affine = eye(3)
     tx = affreg.optimize(static_brain_cropped, moving_brain_cropped, rigid, params0, static_grid2world=g2w,
                          moving_grid2world=g2w, starting_affine=starting_affine)
 
-    moving_[moving_ == moving_.min()] = fill_value
-    tx.domain_grid2world[:2, -1] = -coms
+    
+    tx.domain_grid2world[:2, -1] = -brain_position
     warped = tx.transform(moving_, sampling_grid_shape=moving_.shape)
+    
     return warped, tx
 
 
@@ -54,7 +54,6 @@ def get_cropped_fish(image, phi, dydx, crop_window):
 
     from skimage.transform import rotate
     from numpy import roll, round, array, rad2deg
-    from skimage.morphology import remove_small_objects
 
     shifted = roll(image, round(dydx).astype('int'), axis=(0, 1))
     rotated = rotate(shifted, angle=rad2deg(phi), mode='wrap', preserve_range=True, order=3)
@@ -64,18 +63,15 @@ def get_cropped_fish(image, phi, dydx, crop_window):
 
     # we take the transpose to cancel the effect of this kind of indexing
     crop = rotated[mid[0] + window_y, mid[1] + window_x].T
-    # since we're cropped, we don't need to worry too much about about background removal
-    # this may let some crap through, but that's ok
-    mask = remove_small_objects(crop > long_tail_threshold(crop, shrink_factor=.3), min_size=100)
 
-    return (crop * mask).astype('int16')
+    return crop
+    
 
 
 # point the tail of the fish toward the origin, parallel to the x-axis
 # im must be a binarized fish mask
-def orient_tail(im):
-    from numpy import pi, array, abs, argmin
-    from scipy.ndimage.measurements import center_of_mass
+def orient_tail(im, brain_center, body_center, brain_size=20):
+    from numpy import pi, array, abs, argmin, eye
     from scipy.sparse import issparse
 
     im_ = im.copy()
@@ -83,29 +79,21 @@ def orient_tail(im):
     if issparse(im_):
         im_ = array(im_.todense())
 
+    tform = eye(3)
+    dydx = (0,0)
     phi = 0
-    dydx = (0, 0)
-
-    # if we can't find a fish or a brain, return all 0s
-    if not im_.any():
-        return phi, dydx
-
-    brain = get_brain(im_, return_sparse=False)
-
-    # if we can't find a fish or a brain, return all 0s
-    if not brain.any():
-        return phi, dydx
-
-    raw_com = array(center_of_mass(im_))
-    brain_com = array(center_of_mass(brain))
-    brain_center = remotest_point(im_)
     y_, x_ = brain_center
-
-    tail_y, tail_x = brain_com - raw_com
+    # assume the region in the image around the brain center is the brain
+    brain = im_[y_-brain_size : y_ + brain_size, x_ - brain_size : x_ + brain_size]
+    if not brain.any():
+        return dydx, phi
+        
+    tail_y, tail_x = brain_center - body_center
 
     brain_tail_angle = cart2pol(tail_x, tail_y)[-1]
 
     # Eigenvector of largest eigenvalue corresponds to orientation of the long axis of the brain
+    # unless we have eyes along with the brain, in which case we want the second largest eigenvalue
     evals, evecs = eigenvalues(brain)
     brain_angle = cart2pol(evecs[0, 0], evecs[1, 0])[-1]
     candidate_phis = array([brain_angle, brain_angle + pi])
@@ -117,5 +105,22 @@ def orient_tail(im):
 
     # this vector translates the center of the brain to the center of the image
     dydx = (im_.shape[0] / 2) - y_, (im_.shape[1] / 2) - x_
+    
+    return dydx, phi
+    
 
-    return phi, dydx
+def centered_rotation(image, rotation_center, new_center, phi):
+    """
+    Return an affine matrix for rotating an image around a center point, then translating the center to a new point
+    """
+    from numpy import array, matrix
+    from skimage.transform import warp, AffineTransform
+    origin_y, origin_x = rotation_center
+    shift_y, shift_x = new_center
+    tf_rotate = AffineTransform(rotation=phi)
+    tf_shift = AffineTransform(translation=[-origin_x, -origin_y])
+    tf_shift_inv = AffineTransform(translation=[shift_x, shift_y])
+    params = (tf_shift + (tf_rotate + tf_shift_inv)).params
+    tform = matrix(params).I
+            
+    return tform
